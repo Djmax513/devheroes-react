ec6f5a26f55ae0fa58a52bead8de7b2f0685325e
Gabriel Macedo <gabriel.macedo@agenciam3.com>
2023-10-12 22:03:37 -0300

    feat maps

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,133 +1,107 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View, ScrollView, SafeAreaView, StatusBar } from 'react-native'
-import { Box, Image, Text, Flex, Pressable, Center, VStack, Stack } from "native-base";
+import React, { useState, useRef, useEffect } from "react";
+import { View } from "react-native";
 import MapView from 'react-native-maps';
 import {
-    requestForegroundPermissionsAsync,
-    getCurrentPositionAsync,
-    watchPositionAsync,
-    LocationAccuracy
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  watchPositionAsync,
+  LocationAccuracy
 } from 'expo-location'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import SearchAutocomplete from "./components/SearchAutocomplete"
+import MapViewDirections from 'react-native-maps-directions';
 import { GOOGLE_API_KEY } from "../../../enviroment";
-
-function HomeHeader() {
-    return(
-        <>
-        <Flex direction='row' alignItems='center' bg={`primary.500`} p='6' safeAreaTop>
-            <Image size={84} borderRadius={100} source={require("../../../assets/mock-user-profile.png")} alt="Perfil"/>
-            <Box ml='5' w='70%' color='white'>
-                <Text color='white' fontSize='lg' numberOfLines={1}>Olá Amanda,</Text>
-                <Text color='white' fontSize='sm'>bem vindo de volta!</Text>
-            </Box>
-        </Flex>
-        {/* <Image size={30} w='100%' source={require("../../../assets/header-shape.png")} alt="Perfil"/> */}
-        </>
-    )
-}
-
-function RaceSearchInput({ title }) {
-    return (
-        <Box flex={1}>
-            <GooglePlacesAutocomplete
-                placeholder={title}
-                style={styles.searchContainer}
-                fetchDetails
-                onPress={(data, details = null) => {
-                    // 'details' is provided when fetchDetails = true
-                    console.log(data, details);
-                }}
-                query={{
-                    key: GOOGLE_API_KEY,
-                    language: 'pt-BR',
-                }}
-            />
-        </Box>
-    )
-}
+import RaceDetails from "./components/RaceDetails"
 
 export default function HomePage() {
-    // const [raceStartPoint, setRaceStartPoint] = useState()
-    // const [raceEndPoint, setRaceEndPoint] = useState()
-    const [location, setLocation] = useState(null)
+    const [location, setLocation] = useState(null);
+    const [destination, setDestination] = useState(null);
+    const [origin, setOrigin] = useState(null);
+    const [mapView, setMapView] = useState();
+
+    const map = useRef();
+
+    async function fitMapToDirections() {
+        map.current.fitToCoordinates([
+            destination,
+            origin
+        ], {
+            edgePadding: {
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            },
+          });
+    }
 
     async function requestLocationPermissions() {
-        const { granted } = await requestForegroundPermissionsAsync()
+        const { granted } = await requestForegroundPermissionsAsync();
 
-        if(granted) {
-            const currentPosition = await getCurrentPositionAsync()
+        if (granted) {
+            const currentPosition = await getCurrentPositionAsync();
         }
     }
 
     useEffect(() => {
         requestLocationPermissions();
 
-        watchPositionAsync({
-            accuracy: LocationAccuracy.Highest,
-            timeInterval: 1000,
-            distanceInterval: 1
-        }, (response) => {
-            setLocation(response)
-        })
-    }, [])
+        watchPositionAsync(
+            {
+                accuracy: LocationAccuracy.Highest,
+                timeInterval: 1000,
+                distanceInterval: 1,
+            },
+            (response) => {
+                // console.log("@location response", response)
+                setLocation({
+                    latitude: response.coords.latitude,
+                    longitude: response.coords.longitude,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005
+                });
+                setOrigin({
+                    latitude: response.coords.latitude,
+                    longitude: response.coords.longitude,
+                })
+            }
+        );
+    }, []);
 
     return (
-        <> 
-            <SafeAreaView style={styles.container}>
-            <ScrollView style={styles.scrollView}>
-                <Box>
-                    <HomeHeader />
-                </Box>
-                <VStack>
-                    <Box width={'100%'} elevation={4} bg={'red.100'}>
-                        <RaceSearchInput title="title 1" />
-                        <RaceSearchInput title="title 2" />
-                    </Box>
-                </VStack>
-                <Box p={5}>
-                    <Pressable style={styles.mapContainer}>
-                        {location && (
-                            <MapView
-                            style={styles.map}
-                            initialRegion={{
-                                latitude: location.coords.latitude,
-                                longitude: location.coords.longitude,
-                                latitudeDelta: 0.005,
-                                longitudeDelta: 0.005
-                            }}
+        <View>
+        {location !== null && (
+            <MapView
+            style={{width: "100%", height: "100%"}}
+                initialRegion={location}
+                loadingEnabled
+                showsUserLocation
+                userLocationCalloutEnabled={false}
+                showsMyLocationButton={false}
+                showsCompass={false}
+                ref={map}
+                >
+                {destination !== null && (
+                    <>
+                        <MapViewDirections
+                            origin={origin}
+                            destination={destination}
+                            apikey={GOOGLE_API_KEY}
+                            strokeWidth={4}
+                            strokeColor="blue"
+                            mode={'TRANSIT'}
+                            onReady={() => fitMapToDirections()}
                             />
-                        )}
-                    </Pressable>
-                </Box>
-            </ScrollView>
-            </SafeAreaView>
-        </>
-    )
-}
+                    </>
+                )}
+            </MapView>
+        )}
 
-const styles = StyleSheet.create({
-    map: {
-        width: '100%',
-        height: 250
-    },
-    mapContainer: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        // elevation: 5,
-        borderRadius: 20,
-        overflow: 'hidden',
-    },
-    searchContainer: {
-        backgroundColor: 'red',
-    },
-    container: {
-        flex: 1,
-      },
-    scrollView: {
-    },
-  });
+            <SearchAutocomplete setDestination={setDestination} />
+            {destination !== null && (
+                <>
+                </>
+            )}
+            <RaceDetails />
+        </View>
+    );
+}
